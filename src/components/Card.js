e4d3ee1a25f2659ca5e4aacd31f3b067771ffffa
Faisal Ahmed <faisalahmed99240@gmail.com>
2023-11-05 23:37:00 +0600

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,54 @@
+import { useRef} from 'react'
+import { useDrag, useDrop } from 'react-dnd'
+import { ItemTypes } from './ItemTypes'
+export const Card = ({ id, image, index, moveCard,handleCheckboxChange }) => {
+  const ref = useRef(null)
+  const [{ handlerId }, drop] = useDrop({
+    accept: ItemTypes.CARD,
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      }
+    },
+    hover(item, monitor) {
+      if (!ref.current) {
+        return
+      }
+      const dragIndex = item.index
+      const hoverIndex = index
+      if (dragIndex === hoverIndex) {
+        return
+      }
+      const hoverBoundingRect = ref.current?.getBoundingClientRect()
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+      const clientOffset = monitor.getClientOffset()
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return
+      }
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return
+      }
+      moveCard(dragIndex, hoverIndex)
+      item.index = hoverIndex
+    },
+  })
+  const [{ isDragging }, drag] = useDrag({
+    type: ItemTypes.CARD,
+    item: () => {
+      return { id, index }
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  })
+  const opacity = isDragging ? 0 : 1
+  drag(drop(ref))
+  return (
+    <div ref={ref} style={{opacity }} data-handler-id={handlerId}>
+      <input type="checkbox" name="image" id ={id} onChange={(e) => handleCheckboxChange(e.target.checked, id)} />
+      <img className='img-fluid' src={image} alt="" srcset="" />
+    </div>
+  )
+}
---
+++ src/components/Container.js
@@ -0,0 +1,124 @@
+import { useCallback, useEffect, useState} from 'react';
+import { Card } from './Card';
+import '../components/style.css';
+import data from '../data';
+import Swal from 'sweetalert2'
+import FadeLoader from "react-spinners/FadeLoader";
+
+export const Container = () => {
+
+    const [fileCount, setFileCount] = useState(0);
+    const [checkedEle, setCheckedEle] = useState([]);
+    const [cards, setCards] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setCards(data);
+            setIsLoading(false);
+        }, 2000); 
+    }, []);
+
+    const handleCheckboxChange = (event, id) => {
+        if (event) {
+            setCheckedEle([...checkedEle, id]);
+            const parentDiv = document.getElementById(id).parentElement;
+            parentDiv.classList.add("selected");
+        } else {
+            let filterArray = checkedEle.filter((item, index) => { return item !== id });
+            setCheckedEle(filterArray);
+            const parentDiv = document.getElementById(id).parentElement;
+            parentDiv.classList.remove("selected");
+
+        }
+        setFileCount(event ? fileCount + 1 : fileCount - 1);
+    };
+
+    const handleDeleteFiles = () => {
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: 'btn btn-success',
+                cancelButton: 'btn btn-danger me-2'
+            },
+            buttonsStyling: false
+        })
+
+        swalWithBootstrapButtons.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete!',
+            cancelButtonText: 'No, cancel!',
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                swalWithBootstrapButtons.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                )
+                let cardFilter = cards;
+                checkedEle.forEach((item) => {
+                    cardFilter = cardFilter.filter((cardItem) => cardItem.id !== item);
+                });
+                setCards(cardFilter);
+                setFileCount(0);
+            } else if (
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+                swalWithBootstrapButtons.fire(
+                    'Cancelled',
+                    'Your file is safe :)',
+                    'error'
+                )
+            }
+        })
+    };
+
+    const moveCard = useCallback((dragIndex, hoverIndex) => {
+        const newCards = [...cards];
+        const draggedCard = newCards[dragIndex];
+        newCards.splice(dragIndex, 1);
+        newCards.splice(hoverIndex, 0, draggedCard);
+        setCards(newCards);
+    }, [cards]);
+
+    return <div>
+        {isLoading ? (
+            <div className="loader">
+                <FadeLoader height={15} color={'rgb(54, 215, 183)'} loading={isLoading} />
+            </div>
+        ) : (
+            <div className='d-flex align-items-center justify-content-center bg-custom-secondary'>
+                <div className='image-gallery'>
+                    <div className='d-flex justify-content-between px-5 py-2'>
+
+                        {fileCount === 0 && (
+                            <h5>Gallery</h5>
+                        )}
+
+                        {fileCount > 0 && (
+                            <div className='d-flex align-items-center'>
+                                <input type="checkbox" name="image2" id="image2" checked={fileCount > 0} />
+                                <h5 className='ps-3'>{fileCount <= 1 ? `${fileCount} File Selected` : `${fileCount} Files Selected`}</h5>
+                            </div>
+                        )}
+                        {fileCount > 0 && (
+                            <h6 className='text-danger cursor-pointer' onClick={handleDeleteFiles}>{fileCount <= 1 ? `Delete file` : `Delete files`}</h6>
+                        )}
+                    </div>
+                    <hr />
+                    <div className='container' >
+                        {cards?.map((card, i) => (
+                            <div key={card.id} className={i === 0 ? 'item1 cursor-grab' : 'cursor-grab'}>
+                                <Card key={card.id} index={i} id={card.id} image={card.image} moveCard={moveCard} handleCheckboxChange={handleCheckboxChange} />
+                            </div>
+                        ))}
+                    </div>
+
+                </div>
+            </div>
+        )}
+    </div>;
+};
